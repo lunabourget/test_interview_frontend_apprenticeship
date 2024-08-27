68d4fe19ec16405a736e7693624a12154b54a2fa
luna <lunabourget49@gmail.com>
2024-08-27 14:20:56 +0200

    first task react greetings done

---
+++ react/taskOne/taskOne.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+interface GreetingProps {
+  name: string;
+}
+
+/**
+*  Returns all even numbers from an input array
+* @param name
+* @return string
+*/
+const Greeting: React.FC<GreetingProps> = ({ name }) => {
+  return (
+    <h1>Hello, {name}!</h1>
+  );
+};
+
+export default Greeting;
